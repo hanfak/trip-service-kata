eb3d4cde9175ea08033fc4f021281fd3d7cea782
Hanif Fakira <fakira.work@gmail.com>
2018-07-04 17:14:31 +0100

    refactored: - extracted predicate from filter - removed init of array from field to in orElse - inverted conditional to checking for not logged in and extracting logic out of if statement

--- src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
+++ src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
@@ -2,6 +2,7 @@ package org.craftedsw.tripservicekata.trip;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
 import org.craftedsw.tripservicekata.user.User;
@@ -9,28 +10,34 @@ import org.craftedsw.tripservicekata.user.UserSession;
 
 public class TripService {
 
-    private List<Trip> tripList = new ArrayList<>();
-
     public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
         User loggedUser = loggedUser();
 
-        if (loggedUser != null) {
-            return user.getFriends().stream()
-                    .filter(friend -> friend.equals(loggedUser))
-                    .findFirst()
-                    .map(friend -> findTripsByUser(user))
-                    .orElse(tripList);
-        } else {
+        checkLoggedUSerIsLoggedIn(loggedUser);
+
+        return user.getFriends().stream()
+                .filter(isUserFriendsWithLoggedInUser(loggedUser))
+                .findFirst()
+                .map(friend -> findTripsByUser(user))
+                .orElse(new ArrayList<>());
+    }
+
+    private Predicate<User> isUserFriendsWithLoggedInUser(User loggedUser) {
+        return friend -> friend.equals(loggedUser);
+    }
+
+    private void checkLoggedUSerIsLoggedIn(User loggedUser) {
+        if (loggedUser == null) {
             throw new UserNotLoggedInException();
         }
     }
 
-    protected List<Trip> findTripsByUser(User user) {
-        return TripDAO.findTripsByUser(user);
-    }
-
     protected User loggedUser() {
         return UserSession.getInstance().getLoggedUser();
+    }
+
+    protected List<Trip> findTripsByUser(User user) {
+        return TripDAO.findTripsByUser(user);
     }
 
 }
