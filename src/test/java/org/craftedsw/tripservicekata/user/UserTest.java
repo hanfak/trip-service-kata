82f299c625892f97170b39bf8dd4eeb6d2b2c87c
Hanif Fakira <fakira.work@gmail.com>
2018-07-04 17:44:34 +0100

    created isFriendsWith method for user

--- src/test/java/org/craftedsw/tripservicekata/user/UserTest.java
+++ src/test/java/org/craftedsw/tripservicekata/user/UserTest.java
@@ -1,5 +1,25 @@
 package org.craftedsw.tripservicekata.user;
 
-public class UserTest {
+import org.assertj.core.api.WithAssertions;
+import org.junit.Test;
 
+
+public class UserTest implements WithAssertions {
+    @Test
+    public void isFriendsWithShouldBeTrue() {
+        User john = new User();
+        User bob = new User();
+
+        john.addFriend(bob);
+
+        assertThat(john.isFriendsWith(bob)).isTrue();
+    }
+
+    @Test
+    public void isNotFriendsWithShouldBeFalse() {
+        User john = new User();
+        User bob = new User();
+
+        assertThat(john.isFriendsWith(bob)).isFalse();
+    }
 }
